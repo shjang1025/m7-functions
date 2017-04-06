551ed7cce1b36ba21b5d630ee5adc2edc9f0fbe1
shjang1025 <shjang@uw.eduexit>
2017-04-05 19:42:34 -0700

    lab1

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -21,24 +21,37 @@ CompareLength(a,b)
 # "Your second vector is longer by N elements"
 
 DescribeDifference <- function(c,d) {
-  n <- abs(length(c) - length(d))
-  if(length(c) > length(d)) {
-    c1 <- paste("Your first vector is longer by ", n, " elements") 
-    return(c1) 
-  } elseif (length(c) < length(d)) {
-    c2 <- paste("Your second vector is longer by ", n, " elements")
-    return(c2)
+  l <- length(c) - length(d)
+  if(l > 0) {
+    c1 <- paste("Your first vector is longer by ", l, " elements") 
   } else {
-    c3 <- paste("Both vectors have ", length(c), " elements")
-    return(c3)
+    c1 <- paste("Your second vector is longer by ", -l, " elements")
   }
+  return(c1)
 }
 
 # Pass two vectors to your `DescribeDifference` function
 c <- c(3,6,4,1,3,4,9,1,3)
-b <- c(0,1,0,5,4,9,6,6,0,0,8)
+d <- c(0,1,0,5,4,9,6,6,0,0,8)
 DescribeDifference(c,d)
 
 ### Bonus ###
 
 # Rewrite your `DescribeDifference` function to tell you the name of the vector which is longer
+
+DescribeDifference <- function(a,b) {
+  l <- length(a) - length(b)
+  if(l > 0) {
+    vector.name <- deparse(substitute(a))
+    statement <- paste("Vector ", vector.name)
+  }else {
+    vector.name <- deparse(substitute(b))
+    statement <- paste("Vector ", vector.name)
+  }
+  return(statement)
+}
+
+a <- c(2,333,6,4,5,2)
+b <- c(5,6,7,8,3,2,2,3,45,6,6,7,8,8,9)
+
+DescribeDifference(a,b)
